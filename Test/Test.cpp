9285d568eced9136ec52bb120a9f86d004c984a7
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-02-28 17:47:35 +0530

    tried operator overloading

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+bool compare(bool x) {
+	return x;
+}
+
+bool compare(int x) {
+	if (x >= 3) {
+		cout << "number is less than 3" << endl;
+		return true;
+	}
+	else {
+		cout << "number is less than 3" << endl;
+		return false;
+	}
+}
+
 int main()
 {
+	compare(true);
+	compare(32);
 	return 0;
 }
